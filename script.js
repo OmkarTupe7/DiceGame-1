4dd3df07cfde484c05e79aff095bc9631a8a40b3
MANALI NITIN JAGTAP <102221679+ManaliJagtap49@users.noreply.github.com>
2022-07-05 13:30:23 -0700

    uploaded js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var randomNumber1 = Math.floor(Math.random() *6) +1;
+var randomNumber1 = Math.floor(Math.random() *6)+1;
 var randomDiceImage ="dice" + randomNumber1 + ".png";
 var randomImageSource = "image/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
